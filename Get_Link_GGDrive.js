294a6c1f1407d0543a29bd0c6f8e56ac08b9c38d
Le Trong Hieu <eotihvn@gmail.com>
2021-09-22 11:29:50 +0700

    update copy now

--- Get_Link_GGDrive.js
+++ Get_Link_GGDrive.js
@@ -1,12 +1,19 @@
 function getLinkGgDrive(input) {
     const BASE_URL = "https://drive.google.com/uc?export=download&id="
     var result = BASE_URL + input;
-    document.getElementById("data2").href = result;
-    document.getElementById("data2").innerText = result;
+    // document.getElementById("data2").href = result;
+    // document.getElementById("data2").innerText = result;
+    document.getElementById("myInput").value = result;
 }
 function splitLink() {
     var dataInput = document.getElementById("link").value;
     var url = dataInput.split('/');
     var resultID = url[url.length - 2];
     getLinkGgDrive(resultID)
+}
+function copyText() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copy: "+ copyText.value)
 }
