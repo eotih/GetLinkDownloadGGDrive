fe6727bf94dc639bcdf101a3b9571cf26bf426d9
Le Trong Hieu <eotihvn@gmail.com>
2021-09-24 10:59:21 +0700

    thêm chức năng view

--- Get_Link_GGDrive.js
+++ Get_Link_GGDrive.js
@@ -1,15 +1,26 @@
-function getLinkGgDrive(input) {
+function getLinkDownloadGgDrive(input) {
     const BASE_URL = "https://drive.google.com/uc?export=download&id="
     var result = BASE_URL + input;
     // document.getElementById("data2").href = result;
     // document.getElementById("data2").innerText = result;
     document.getElementById("myInput").value = result;
 }
+function getLinkViewGgDrive(input) {
+    const BASE_URL = "https://drive.google.com/uc?export=view&id="
+    var result = BASE_URL + input;
+    document.getElementById("myInput").value = result;
+}
 function splitLink() {
     var dataInput = document.getElementById("link").value;
     var url = dataInput.split('/');
     var resultID = url[url.length - 2];
-    getLinkGgDrive(resultID)
+    getLinkDownloadGgDrive(resultID)
+}
+function splitLink2() {
+    var dataInput = document.getElementById("view").value;
+    var url = dataInput.split('/');
+    var resultID = url[url.length - 2];
+    getLinkViewGgDrive(resultID)
 }
 function copyText() {
     var copyText = document.getElementById("myInput");
